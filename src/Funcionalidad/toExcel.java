f088fd53ef628c00477c6daab315609af0e0d8e1
Ivan Riveros <riverosgarciaivan@gmail.com>
2021-01-12 13:54:17 -0600

    Generar arhivo CSV(prueba)

--- src/Funcionalidad/toExcel.java
+++ src/Funcionalidad/toExcel.java
@@ -3,10 +3,12 @@ package Funcionalidad;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.PreparedStatement;
+import java.io.PrintWriter;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,10 +19,8 @@ import jxl.format.Alignment;
 import jxl.format.Border;
 import jxl.format.BorderLineStyle;
 import jxl.format.Colour;
-import jxl.format.UnderlineStyle;
 import jxl.format.VerticalAlignment;
 import jxl.write.Label;
-import jxl.write.Number;
 import jxl.write.WritableCellFormat;
 import jxl.write.WritableFont;
 import jxl.write.WritableImage;
@@ -30,11 +30,12 @@ import jxl.write.WriteException;
 import modelo.dao.ReporteDAO;
 
 public class toExcel {
-    
+
+    Date hoy = new Date();
     File file = new File("output.xls");
     ReporteDAO reporteDAO = new ReporteDAO();
 
-    public void formatearArchivo() {
+    public void formatearArchivoXLSX() {
         int row = 1, column = 1;
 
         WritableFont titulo1 = new WritableFont(WritableFont.ARIAL, 16, WritableFont.BOLD);
@@ -42,6 +43,7 @@ public class toExcel {
         WritableFont letraSencilla = new WritableFont(WritableFont.ARIAL, 10, WritableFont.NO_BOLD);
         try {
             WritableCellFormat celdaSencilla = new WritableCellFormat(letraSencilla);
+
             WritableCellFormat celdaSencillaNegrita = new WritableCellFormat(letraNegrita);
 
             WritableCellFormat celdaGrisBorde = new WritableCellFormat(letraNegrita);
@@ -90,7 +92,7 @@ public class toExcel {
             excelSheet.addCell(new Label(column, row, "(7)-REGISTRO DE SALIDAS CORRESPONDIENTE AL  DIA:", celdaSencillaNegrita));
             excelSheet.addCell(new Label(column + 4, row, "01/04/2020", celdaSencilla));
 
-            java.io.File imageFile = new java.io.File("logo.png");
+            File imageFile = new File("logo.png");
             BufferedImage input = ImageIO.read(imageFile);
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             ImageIO.write(input, "PNG", baos);
@@ -105,7 +107,7 @@ public class toExcel {
             excelSheet.setColumnView(column + 3, 45);
             excelSheet.setColumnView(column + 4, 30);
             excelSheet.setColumnView(column + 5, 15);
-            excelSheet.setColumnView(column + 6, 15);
+            excelSheet.setColumnView(column + 6, 10);
             excelSheet.setColumnView(column + 7, 13);
             excelSheet.setColumnView(column + 8, 13);
             excelSheet.setColumnView(column + 9, 12);
@@ -145,6 +147,35 @@ public class toExcel {
             System.out.println("Error al escribir en el archivo: " + ex);
         } catch (IOException ex) {
             System.out.println("Error al escribir en el disco: " + ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(toExcel.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void generateCSVfile() {
+        try {
+            PrintWriter pw = new PrintWriter(new File("16 DE ABRIL.csv"));
+            StringBuilder sb = new StringBuilder();
+
+            sb.append("FORMATO DE SALIDAS DE AUTOBUS EN CENTRAL \n");
+            sb.append("Hoja de Registro No. 1 \n");
+            sb.append("POBLACION:,,,TULANCINGO\n");
+            sb.append("NOMBRE DE LA  CENTRAL O TERMINAL DE AUTOBUSES:,,,CENTRAL DE AUTOBUSES DE TULANCINGO S.A. DE C.V.\n");
+            sb.append("DIRECCION:,,,CARRETERA MEXICO-TUXPAN KM. 143\n");
+            sb.append(",,,COL. NUEVO TULANCINGO. C.P. 43612\n");
+            sb.append("FECHA DE ELABORACIÓN:,,,17/04/2020\n");
+            sb.append("REGISTRO DE SALIDAS CORRESPONDIENTE AL DIA:,,,jueves, 16 de abril de 2020\n\n");
+            
+            sb.append(",No.,HORA DE SALIDA,DESTINO,EMPRESA,CORRIDA EXTRA,NUMERO ECONOMICO DE AUTOBUS,N° DE PASAJEROS,N° DE SALIDA DE AUTOBUS\n\n");
+            ResultSet res = null;
+            while(res.next()){
+                
+            }
+            res.close();
+            pw.write(sb.toString());
+            pw.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("error: " + e);
         } catch (SQLException ex) {
             Logger.getLogger(toExcel.class.getName()).log(Level.SEVERE, null, ex);
         }
